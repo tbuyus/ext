e46bf495db473479392b53dfd1254d08bb94511a
tbuyus <tbuyus@tbuyus-ME05.(none)>
2014-12-03 09:12:24 +0000

    added tradspell word to output

--- db/chrome/content/sqlite-dictionary-scripts.js
+++ db/chrome/content/sqlite-dictionary-scripts.js
@@ -72,7 +72,7 @@ function DictionaryEntryRaw(word){
  statement.reset;
  value=value.replace(/ /g,"");
  value=revertToOriginalCase(value,ceys);
- pairvalue=value + "\/\/"+ wordrank;
+ pairvalue=value + "\/\/"+ wordrank+"\/\/"+word;
  return pairvalue;
 }
 
